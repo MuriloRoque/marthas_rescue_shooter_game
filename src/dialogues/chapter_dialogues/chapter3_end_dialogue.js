bcae069c4f0927bb2e299ca2ac69043b4876b6a4
Murilo Roque <muriloengqui@gmail.com>
2020-07-06 11:13:29 -0300

    add chapter 3 end dialogue

---
+++ src/dialogues/chapter_dialogues/chapter3_end_dialogue.js
@@ -0,0 +1,93 @@
+export default class Chapter3EndDialogueScene extends Phaser.Scene {
+  constructor () {
+    super('Chapter3EndDialogue');
+  }
+
+  preload () {
+  }
+
+  create () {
+    this.add.image(400, 300, 'swamp').setDisplaySize(800, 600);
+    var dialog = this.rexUI.add.dialog({
+      x: 400,
+      y: 300,
+      width: 500,
+
+      background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x466D1D),
+
+      title: createLabel(this, 'Chapter 3 End').setDraggable(),
+
+      content: createLabel(this, 'Mission Complete!'),
+
+      description: createLabel(this, "Martha: 'This is the third one and I still got nothing.\nHuh, what's that written on the wall?'\nWall: 'Your destiny is in the sleepless mountain'\nMartha: 'Hmmm'"),
+
+      actions: [
+          createLabel(this, 'Next')
+      ],
+
+      space: {
+          left: 20,
+          right: 20,
+          top: -20,
+          bottom: -20,
+          title: 25,
+          content: 25,
+          description: 25,
+          choices: 25,
+          leftToolbarItem: 5,
+          toolbarItem: 5,
+          choice: 15,
+          action: 15,
+      },
+
+      expand: {
+          title: false,
+      },
+
+      align: {
+          title: 'center',
+          actions: 'right',
+      },
+
+      click: {
+          mode: 'release'
+      }
+  })
+      .setDraggable('background')
+      .layout()
+      .popUp(1000)
+
+  this.print = this.add.text(0, 0, '');
+  dialog
+      .on('button.click', function () {
+         this.scene.start('Chapter3Bonus');
+      }, this)
+      .on('button.over', function (button) {
+          button.getElement('background').setStrokeStyle(1, 0xffffff);
+      })
+      .on('button.out', function (button) {
+          button.getElement('background').setStrokeStyle();
+      });
+  }
+  
+}
+
+var createLabel = function (scene, text) {
+  return scene.rexUI.add.label({
+      width: 40,
+      height: 40,
+
+      background: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x607D3B),
+
+      text: scene.add.text(0, 0, text, {
+          fontSize: '24px'
+      }),
+
+      space: {
+          left: 10,
+          right: 10,
+          top: 10,
+          bottom: 10
+      }
+  });
+}
